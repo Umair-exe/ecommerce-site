0680f1089bb1b8d741d99bd454972d282896bdd3
umair-exe <umair.shabir99@gmail.com>
2022-09-26 00:49:04 +0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/vendor-shop',(req,res,next) => {
+  res.render('shop');
+})
+router.get('/trade',(req,res,next) => {
+  res.render('trade');
+})
+router.get('/bidding',(req,res,next) => {
+  res.render('bidding');
+})
+router.get('/cart',(req,res,next) => {
+  res.render('cart');
+})
+router.get('/checkout',(req,res,next) => {
+  res.render('checkout');
+})
+router.get('/contact',(req,res,next) => {
+  res.render('contact');
+})
+router.get('/login',(req,res,next) => {
+  res.render('login');
+})
+router.get('/register',(req,res,next) => {
+  res.render('register');
+})
+
+module.exports = router;
